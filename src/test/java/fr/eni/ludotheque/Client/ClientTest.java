eb252dd3993a5b0dc89a3eea427531380e888a7d
mrichard12023 <mylan.richard2023@campus-eni.fr>
2025-04-15 16:41:17 +0200

    edit TU

--- src/test/java/fr/eni/ludotheque/Client/ClientTest.java
+++ src/test/java/fr/eni/ludotheque/Client/ClientTest.java
@@ -1,4 +1,18 @@
 package fr.eni.ludotheque.Client;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class ClientTest {
+
+    @Test
+    void testAddClient() {
+
+    }
+
+    @Test
+    void testAddClientWithAdress() {
+
+    }
 }
