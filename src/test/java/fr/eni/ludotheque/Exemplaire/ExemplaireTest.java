a8d1cb3abb33fcb10df87eb6531e19b10de730d2
mrichard12023 <mylan.richard2023@campus-eni.fr>
2025-04-16 09:07:04 +0200

    add package test

---
+++ src/test/java/fr/eni/ludotheque/Exemplaire/ExemplaireTest.java
@@ -0,0 +1,38 @@
+package fr.eni.ludotheque.Exemplaire;
+
+import fr.eni.ludotheque.bo.Adresse;
+import fr.eni.ludotheque.bo.Client;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+@Slf4j
+public class ExemplaireTest {
+
+
+    @Test
+    void testAddClient() {
+        Client client = new Client().builder().nom("nom").prenom("prenom").email("email").noTelephone("noTelephone").build();
+
+        Assert.notNull(client, "Client not null");
+        log.info(client.toString());
+    }
+
+    @Test
+    void testAddClientWithAdress() {
+
+        Client client = new Client().builder()
+                .noClient(1L)
+                .nom("nom")
+                .prenom("prenom")
+                .email("email")
+                .noTelephone("noTelephone")
+                .adresse(new Adresse().builder().rue("rue").codePostal("codePostal").ville("ville").build())
+                .build();
+
+        log.info(client.toString());
+
+    }
+}
